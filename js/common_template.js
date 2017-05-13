a43e40882d11b8dea53c34d5145efb942479abbd
Tuzov <dmy2zov@gmail.com>
2017-05-13 15:59:33 +0300

    header, footer, join us, scroll nav, mobile

---
+++ js/common_template.js
@@ -0,0 +1,22 @@
+$('.day-block').click(function() {
+	$('.day-block').removeClass('active-day');
+	$(this).toggleClass('active-day');
+});
+
+$('.send').click(function() {
+      $('.success').addClass('active');
+      setTimeout(function() {
+            $('.success').removeClass('active').fadeOut();
+      }, 7000);
+      return false;
+});
+
+$('.btn-mobile').click(function() {
+	$(this).toggleClass('active');
+	$('.nav-mobile').toggleClass('active');
+});
+
+$('.nav-mobile').click(function() {
+	$(this).removeClass('active');
+	$('.btn-mobile').removeClass('active');
+});
